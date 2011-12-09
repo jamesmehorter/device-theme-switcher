a96514b072f264a9cbefd43d822ce24df7e19da9
James <jamesmehorter@gmail.com>
2011-12-09 16:19:46 -0500

    Initial commit. Starting development on a wp plugin to switch themes based on the device NOT the browser.

---
+++ device-theme-switcher.php
@@ -0,0 +1,15 @@
+<?php
+	/* 
+		Plugin Name: Device Theme Switcher
+		Plugin URI: http://www.picadesign.com.com
+		Description: Plugin that allows you to set separate themes for desktop, handheld, and tablet devices
+		Author: James Mehorter @ Pica Design
+		Version: 0.1
+		Author URI: http://www.jamesmehorter.com
+	*/
+	
+	
+	//$isiPad = (bool) strpos($_SERVER['HTTP_USER_AGENT'],'iPad');
+	
+	include('views/admin-plugin-display.php');
+?>
