d7713e27dae4bd5d0734488fa42cbd02b7748768
James Mehorter <jamesmehorter@gmail.com>
2011-12-10 20:24:39 -0500

    Continued build out of the core plugin class. Separated the admin page output into an external php file. Having issues with the settings saving.

---
+++ dts_admin_output.php
@@ -0,0 +1,54 @@
+<?php
+	//Gather data that we'll use on the plugin settings page
+	
+	$themeList = array_keys(get_themes());
+	natcasesort($themeList);
+	$device_theme = get_option('dts_device_themes');
+	
+	print_r($device_theme);
+	
+	
+?>
+<!-- This is the html page that displays on the plugin settings page -->
+<style type="text/css">
+    #dts_settings_page_wrapper {
+        margin: 15px 0 0 15px ;
+    }
+</style>
+<div id="dts_settings_page_wrapper">
+    <form method="post" action="options.php">
+        <?php settings_fields('dts_settings'); ?>
+        <table>
+            <tr>
+                <td colspan="2">
+                    <h3>Select your device-specific themes below</h3>
+                </td>
+            </tr><tr>
+                <td>
+                    <label for="device_theme[handheld]"><?php _e("Handheld Theme") ?></label>
+                </td><td align="right">
+                    <select name="device_theme[handheld]">
+                        <?php foreach ($themeList as $key => $theme) : ?>
+                            <option name="<?php echo $theme ?>" <?php selected($theme, $device_theme['handheld']) ?>><?php echo $theme ?></option>
+                        <?php endforeach ?>
+                    </select>
+                </td>						
+            </tr><tr>
+                <td>
+                    <label for="device_theme[tablet]"><?php _e("Tablet Theme") ?></label>
+                </td><td align="right">
+                    <select name="device_theme[tablet]">
+                        <?php foreach ($themeList as $key => $theme) : ?>
+                            <option name="<?php echo $theme ?>" <?php selected($theme, $device_theme['tablet']) ?>><?php echo $theme ?></option>
+                        <?php endforeach ?>
+                    </select>
+                </td>
+            </tr>
+                <td colspan="2" align="right">
+                    <br /><br />
+                    <input type="submit" value="<?php _e("Save Device Themes") ?>" />
+                </td>
+            </tr>
+        </table>
+    </form>
+</div>
---
+++ dts_controller.php
@@ -0,0 +1,77 @@
+<?php
+	/** 
+		Plugin Name: Device Theme Switcher
+		Plugin URI: http://www.picadesign.com.com
+		Description: Plugin that allows you to set a separate theme for handheld and tablet devices
+		Author: James Mehorter @ Pica Design
+		Version: 0.1
+		Author URI: http://www.jamesmehorter.com
+	*/
+	
+	
+	//$isiPad = (bool) strpos($_SERVER['HTTP_USER_AGENT'],'iPad');
+	
+	
+	// ------------------------------------------------------------------------
+	// REGISTER HOOKS & CALLBACK FUNCTIONS:                                    
+	// ------------------------------------------------------------------------
+	register_activation_hook(__FILE__, array('device_theme_switcher', 'add_defaults'));
+	register_uninstall_hook(__FILE__, array('device_theme_switcher', 'remove'));
+	add_action('admin_init', array('device_theme_switcher', 'init') );
+	add_action('admin_menu', array('device_theme_switcher', 'admin_menu'));
+	
+	
+	// ------------------------------------------------------------------------
+	// DEVICE THEME SWITCHER CONTROLLER CLASS                                    
+	// ------------------------------------------------------------------------
+	class device_theme_switcher {
+		
+		// ------------------------------------------------------------------------------
+		// CALLBACK MEMBER FUNCTION FOR: add_action('admin_init', array('device_theme_switcher', 'init') );
+		// ------------------------------------------------------------------------------
+		public function init() {
+			//Register our plugin settings in the wp-options table
+			register_setting('dts_settings', 'dts_device_themes');
+		}//END member function init
+		
+		// ------------------------------------------------------------------------------
+		// CALLBACK MEMBER FUNCTION FOR: register_activation_hook(__FILE__, array('device_theme_switcher', 'add_defaults'));
+		// ------------------------------------------------------------------------------
+		//Set plugin default settings
+		public function add_defaults () {
+		}//END member function add_defaults
+		
+		
+		// ------------------------------------------------------------------------------
+		// CALLBACK MEMBER FUNCTION FOR: add_action('admin_menu', array('device_theme_switcher', 'admin_menu'));
+		// ------------------------------------------------------------------------------
+		public function admin_menu () {
+			//Create the admin menu page
+			add_options_page( __('Device Theme Switcher'), __('Device Themes'), 10, basename(__FILE__), array('device_theme_switcher', 'generate_admin_settings_page'));
+		}//END member function admin_menu
+		
+		
+		// ------------------------------------------------------------------------------
+		// CALLBACK MEMBER FUNCTION FOR: register_uninstall_hook(__FILE__, array('device_theme_switcher', 'remove'));
+		// ------------------------------------------------------------------------------
+		public function remove () {
+			//Remove the plugin and it's settings
+			delete_option('dts_device_themes');
+		}//END member function remove
+		
+		
+		// ------------------------------------------------------------------------------
+		// CALLBACK FUNCTION SPECIFIED IN: add_options_page()
+		// ------------------------------------------------------------------------------
+		//Generate and output the plugin settings page
+		public function generate_admin_settings_page() {
+			print_r($_POST);
+			include('dts_admin_output.php'); 
+		} //END member function generate_admin_settings_page
+		
+	} //END class definition for the device_theme_switcher
+	
+	
+	//Create an object of type device_theme_switcher for executing the plugin
+	//$dts = new device_theme_switcher() ;
+?>
