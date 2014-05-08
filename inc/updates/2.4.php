d060bea37788d328a7dc099b23502169186fff26
James Mehorter <jamesmehorter@gmail.com>
2014-05-07 20:42:28 -0400

    Ensured that during update to 2.4 that default values are preserved. I.e. if the user never adjusted the default session lifetime it's probably safe to update them to the new default value of letting the cookie expire when the browser closes.

--- inc/updates/2.4.php
+++ inc/updates/2.4.php
@@ -10,7 +10,13 @@
     update_option('dts_cookie_name', DTS_Core::build_cookie_name());
 
     //Add the new option using the new name and existing dts_session_lifetime value
-    update_option('dts_cookie_lifespan', get_option('dts_session_lifetime'));
+    $dts_cookie_lifespan = get_option('dts_session_lifetime');
+    //If the session is still 900 seconds / 15 minutes (the default set in version 2.0)
+    //Change that to the new default (0 = until the user closes their browser)
+    if ($dts_cookie_lifespan == 900) $dts_cookie_lifespan = 0 ;
+
+    //Save the new dts_cookie_lifespan option
+    update_option('dts_cookie_lifespan', $dts_cookie_lifespan);
     
     //Remove the old option
     delete_option('dts_session_lifetime');
