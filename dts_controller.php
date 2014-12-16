cefaa82a4d3c554f477c4a1eeb8fc0d8a0d72fc2
James Mehorter <james@10up.com>
2014-12-15 21:50:49 -0500

    Documentation Edits
    Just reading/better documenting current code.

--- dts_controller.php
+++ dts_controller.php
@@ -33,12 +33,12 @@
 	defined( 'ABSPATH' ) OR exit;
 
 	/**
-     * DTS_VERSION constant for use anywhere in WordPress 
+     * DTS_VERSION constant for use anywhere in WordPress
      */
     define( 'DTS_VERSION', '2.9.0' );
     define( 'DTS_URL',     plugin_dir_url( __FILE__ ) );
 	define( 'DTS_PATH',    dirname( __FILE__ ) . '/' );
-	
+
     /**
      * Create one globally-accessible access point for the DTS_Switcher class
      *
@@ -47,29 +47,37 @@
     global $dts ;
 
 	/**
-	 *	
+	 *
 	 * Load the plugin core routines
 	 *
-	 * This is where Device Theme Switcher hooks into the WordPress
-	 * activation, deactivation, unintiall, init, and plugin_action_links
+	 * This is where Device Theme Switcher hooks into the WordPress (allthethings!)
+	 * Everything from activation, deactivation, unintiall, init,
+	 * and other WordPress hooks to build all the plugin functionality.
 	 */
-	
+
 	// Include our reusable singleton class used throughout the plugin
 	include_once( 'includes/class-singleton.php' );
 
 	// Include the core class
+	// Make available all the main plugin functionality (we won't run anything quite yet though..)
 	include_once( 'includes/class-core.php' );
-	
+
 	// Activation: Install any initial settings or run any update routines
 	register_activation_hook( __FILE__, array( 'DTS_Core', 'activate' ) );
-	
+
 	// Deactivation: Run any special routines on deactivation
 	register_deactivation_hook( __FILE__, array( 'DTS_Core', 'deactivate' ) );
-	
+
 	// Uninstall: Remove anything stored in the database
-	register_uninstall_hook( __FILE__, array( 'DTS_Core', 'uninstall' ) );	
-	
+	register_uninstall_hook( __FILE__, array( 'DTS_Core', 'uninstall' ) );
+
 	// Startup
+	//
+	// This is where it all starts on each load..
+	//
+	// DTS_Core::factory() calls init() on itself,
+	// wherein the main plugin functionality is included,
+	// hooks into WordPress, and is executed when needed.
 	add_action( 'plugins_loaded', array( 'DTS_Core', 'factory' ) );
 
 
--- includes/class-core.php
+++ includes/class-core.php
@@ -3,7 +3,7 @@
     defined( 'ABSPATH' ) OR exit;
 
     /**
-     *  
+     *
      * Load the plugin core routines
      *
      * This is where Device Theme Switcher hooks into the WordPress
@@ -31,7 +31,7 @@
                 }
             } // if is_admin
 
-            // Load our plugins includes 
+            // Load our plugins includes
             $this->load_includes();
 
             // Hook into WordPress
@@ -41,8 +41,8 @@
 
         /**
          * Plugin Activation
-         * 
-         * This method is run statically in dts_controller.php 
+         *
+         * This method is run statically in dts_controller.php
          * on the register_activation_hook() function.
          *
          * @internal  Called via register_activation_hook
@@ -70,20 +70,20 @@
                 }
             } // if is_admin
         } // function activate
-        
+
 
         /**
          * Plugin Deactivation
-         * 
-         * This method is run statically in dts_controller.php 
+         *
+         * This method is run statically in dts_controller.php
          * on the register_deactivation_hook() function
          *
          * @return void
          */
         static function deactivate () {
-            
+
             // Do nothing on deactivation
-            
+
         } // function deactivate
 
         /**
@@ -93,7 +93,7 @@
          * @return bool  truthy do we need to install?
          */
         public function need_install () {
-            
+
             // get_current_version returns false when no version is set
             // this is our indicator that we need to run the fresh install routine
             if ( false === $this->get_current_version() ) {
@@ -119,7 +119,7 @@
             // A cookie is stored in your visitors browser when they've
             //  access the website via a device and have since chosen to
             add_option( 'dts_cookie_name', $this->build_cookie_name() );
-            
+
             // When storing the cookie we set a lifespan for it as well.
             // Within the plugin settings admins can adjust this lifespan.
             // By default, with a lifespan of 0, the cookie decays when the
@@ -135,15 +135,15 @@
 
         /**
          * Remove all plugin options stored in the database
-         * 
-         * This method is run statically in dts_controller.php 
+         *
+         * This method is run statically in dts_controller.php
          * on the register_uninstall_hook() function
          *
          * @uses   delete_option()
          * @return void
          */
         static function uninstall () {
-            
+
             //Remove the plugin's settings
             delete_option( 'dts_version' );
             delete_option( 'dts_handheld_theme' );
@@ -156,7 +156,7 @@
 
 
         /**
-         * Load all the files the plugin will need 
+         * Load all the files the plugin will need
          *
          * @see    self::__construct
          * @return null
@@ -166,9 +166,9 @@
             /**
              * Load the plugin admin features
              *
-             * The admin features include the display of the status output in the Dashboard 
+             * The admin features include the display of the status output in the Dashboard
              * 'Right Now' widget. They also create an admin page at Appearance > Device Themes
-             * for the website admin to save the plugin settings 
+             * for the website admin to save the plugin settings
              */
             // include the wp-admin class
             include_once( 'class-wp-admin.php' );
@@ -178,20 +178,20 @@
              *
              * The theme switching utilizes the MobileESP library to detect
              * the browser User Agent and determine if it's a 'handheld' or 'tablet'.
-             * This plugin then taps into the WordPress template and stylesheet hooks 
+             * This plugin then taps into the WordPress template and stylesheet hooks
              * to deliver the alternately set themes in Appearance > Device Themes
              */
             // We only want to tap into the theme filters if a frontend page or an ajax request is being requested
-            
+
             // Include our external device theme switcher class library
             include_once( 'class-switcher.php' );
-            
+
             // Load support for legacy GET variables used in version 1.0.0
             include_once( 'legacy/legacy-get-support.php' );
 
             // Include the template tags developers can access in their themes
             include_once( 'template-tags.php' );
-            
+
             // Load support for legacy classes, methods, functions, and variables
             include_once( 'legacy/legacy-structural-support.php' );
 
@@ -201,14 +201,14 @@
              * The widgets create an option for capable users to place 'View Full Website'
              * and 'Return to Mobile Website' links in their theme sidebars.
              */
-            // Load the widget class 
+            // Load the widget class
             include_once( 'class-widgets.php' );
 
             /**
              * Load the plugin shortcodes
              *
              * The shortcodes allow capable users to place 'View Full Website' and
-             * 'Return to Mobile Website' links in their posts / pages. Register the 
+             * 'Return to Mobile Website' links in their posts / pages. Register the
              * [device-theme-switcher] shortcode and Include our external shortcodes class library
              */
             // load the shortodes class
@@ -219,26 +219,26 @@
 
         /**
          * Hook into WordPress
-         * 
+         *
          * @return null
          */
         public function hook_into_wordpress () {
-            
+
             // Only run the following in the admin, the REAL admin--not admin ajax
             if ( is_admin() && ( ! defined( 'DOING_AJAX' ) || ! DOING_AJAX ) ) {
 
                 // Grab the single instance of the admin class
                 $dts_admin = DTS_Admin::factory();
-                
+
                 // Create our plugin admin page under the 'Appearance' menu
                 add_action( 'admin_menu', array( $dts_admin, 'admin_menu' ), 10, 0 );
-                
+
                 // Check if we need to save any form data that was submitted
                 add_action( 'load-appearance_page_device-themes', array( $dts_admin, 'load' ), 10, 0 );
 
                 // Display a 'Settings' link with the plugin in the plugins list
                 add_filter( 'plugin_action_links', array( $this, 'device_theme_switcher_settings_link' ), 10, 2 );
-                
+
                 // Add admin scripts and styles used to display the admin settings view
                 add_action( 'admin_init', array( $this, 'admin_enqueue_scripts' ) );
 
@@ -251,7 +251,7 @@
 
                 // Hook into the template output function with a filter and change the template delivered if need be
                 add_filter( 'template', array( $dts, 'deliver_template' ), 10, 0 );
-                
+
                 // Hook into the stylesheet output function with a filter and change the stylesheet delivered if need be
                 add_filter( 'stylesheet', array( $dts, 'deliver_stylesheet' ), 10, 0 );
 
@@ -276,7 +276,7 @@
         function admin_enqueue_scripts () {
 
             // Enqueue our JavaScript
-            wp_enqueue_script( 
+            wp_enqueue_script(
                 'device-theme-switcher-admin-scripts', // handle
                 DTS_URL . 'assets/js/device-theme-switcher-admin-scripts.min.js', // srouce
                 array( 'jquery' ), // dependencies
@@ -285,20 +285,20 @@
             );
 
             // Enqueue our Stylesheet
-            wp_enqueue_style( 
+            wp_enqueue_style(
                 'device-theme-switcher-admin-styles', //handle
                 DTS_URL . 'assets/css/device-theme-switcher-admin-styles.css', // source
                 array(), // dependencies
                 DTS_VERSION, //version
-                'all' // media 
+                'all' // media
             );
 
         } // function admin_enqueue_scripts
 
 
         /**
-         * Register our widgets 
-         * 
+         * Register our widgets
+         *
          * For displaying 'View Full Website' and 'Return to mobile website' links
          *
          * @internal  widgets_init action hook
@@ -310,7 +310,7 @@
 
             // Register the 'View Full Website' widget
             register_widget( 'DTS_View_Full_Website' );
-            
+
             // Register the 'Return to Mobile Website' widget
             register_widget( 'DTS_Return_To_Mobile_Website' );
 
@@ -320,23 +320,23 @@
         /**
          * Add a 'Settings' link to the plugin row in the WP-Admin > Plugins page
          *
-         * This method is run statically in dts_controller.php 
+         * This method is run statically in dts_controller.php
          * on the 'plugin_action_links' hook
          *
          * @uses    admin_url
          * @param   array $links   The current plugin links
          * @param   string $file   The main plugin path/filename.php
          * @return  array  $links  After adding in our own
-         */    
+         */
         static function device_theme_switcher_settings_link( $links, $file ) {
             if ( 'device-theme-switcher/dts_controller.php' == $file ) {
 
-                // Insert a new 'Settings' link which points to the 
+                // Insert a new 'Settings' link which points to the
                 // Appearance > Device Themes page
-                $links['settings'] = sprintf( 
-                    '<a href="%s" class="edit"> %s </a>', 
-                    admin_url( 'themes.php?page=device-themes' ), 
-                    __( 'Settings', 'device_theme_switcher' ) 
+                $links['settings'] = sprintf(
+                    '<a href="%s" class="edit"> %s </a>',
+                    admin_url( 'themes.php?page=device-themes' ),
+                    __( 'Settings', 'device_theme_switcher' )
                 );
 
             } // end if
@@ -361,19 +361,19 @@
         static public function build_cookie_name () {
             // Start with the site name for the cookie name
             $cookie_name = get_bloginfo( 'sitename' );
-            
+
             // Remove special characters
-            $cookie_name = preg_replace( '/[^a-zA-Z0-9_%\[().\]\\/-]/s', '', $cookie_name ); 
-            
+            $cookie_name = preg_replace( '/[^a-zA-Z0-9_%\[().\]\\/-]/s', '', $cookie_name );
+
             // Change spaces to hyphens
             $cookie_name = str_replace( ' ', '-', $cookie_name );
-            
+
             // Lowercase everything
             $cookie_name = strtolower( $cookie_name );
-            
+
             // Append some identifying text
             $cookie_name = $cookie_name . '-alternate-theme';
-            
+
             // Return the assembled cookie name
             return $cookie_name;
 
@@ -384,7 +384,7 @@
          * Determine the current plugin version
          *
          * This function grabs the version stored in a WordPress option, however,
-         * pre version 2.0 we never stored the plugin version in an option, so if 
+         * pre version 2.0 we never stored the plugin version in an option, so if
          * there is no stored version we specify it below as '1'
          *
          * @uses   get_option
@@ -394,8 +394,8 @@
 
             // check for the dts_version option (New in Version 2.0)
             $current_version = get_option('dts_version');
-            
-            // If there is no current version we'll just reference 
+
+            // If there is no current version we'll just reference
             // this as version 0.0.0 for the installation process which will
             if ( empty( $current_version ) ) {
                 return false ;
@@ -424,13 +424,13 @@
                 // Careful! This could mean one of two scenarios:
                 // Scenario 1) This is a fresh install with no version yet
                 // Scenario 2) This is an a pre version 2.0.0 install and an update to 2.0.0 is needed
-                // 
+                //
                 // Determine if this is a pre 2.0.0 install
-                // In the first iterations of this plugin, 
+                // In the first iterations of this plugin,
                 // before 2.0.0 we used an option titled 'dts_device'
                 // We can check for it's presence..
                 if ( false === get_option( 'dts_device' ) ) {
-                    // No, this is not a pre 2.0.0 install, 
+                    // No, this is not a pre 2.0.0 install,
                     // That means this is a clean fresh install, no update needed
                     return false ;
                 } else {
@@ -465,14 +465,19 @@
             // Fetch the current plugin version
             $current_version = $this->get_current_version();
 
-            // If no version has been set already that means we're 
-            // updating from the version 1.x series when we did not 
+            // If no version has been set already that means we're
+            // updating from the version 1.x series when we did not
             // store the plugin version in the database
             if ( false === $current_version ) {
                 $current_version = '1.0';
             }
 
             // Array of versions and update routines
+            // A version only has/needs an update routine
+            // when something fairly major changed like a database
+            // option name change and we wanted people to have a smooth
+            // update transition, so we run some 'marrying' code, to rename
+            // an old option name for instance.
             $plugin_versions = array(
                 '1.0'   => '',
                 '2.0'   => 'dts-2.0.php', // Update to version 2.0.0
@@ -490,7 +495,7 @@
             // Loop through the available updates
             // This looping logic will perform any missed updates since the last update was run
             foreach ( $plugin_versions as $version => $version_update_routine_filename ) {
-                
+
                 // Check if the current version is less than the available version
                 if ( version_compare( $current_version, $version, '<' ) ) {
 
@@ -498,11 +503,11 @@
                     if ( ! empty( $version_update_routine_filename ) ) {
 
                         // Yes, this update has a routine we need to run
-                        // we need only include the update routine 
+                        // we need only include the update routine
                         // The update file will run automatically
-                        include_once( DTS_PATH . 'updates/' . $version_update_routine_filename );  
+                        include_once( DTS_PATH . 'updates/' . $version_update_routine_filename );
                     }
-                    
+
                     // Update the current version to reflect the update
                     $current_version = $version;
                 }
