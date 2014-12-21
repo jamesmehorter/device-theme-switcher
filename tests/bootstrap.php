43c8c7ee57bdb3e0697761de41dc7e6f5a64d518
James Mehorter <james@10up.com>
2014-12-21 13:55:17 -0500

    Updated the automatically-created bootstrap to load our odd-named plugin main file

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,7 +6,7 @@ if ( !$_tests_dir ) $_tests_dir = '/tmp/wordpress-tests-lib';
 require_once $_tests_dir . '/includes/functions.php';
 
 function _manually_load_plugin() {
-	require dirname( __FILE__ ) . '/../device-theme-switcher-dev.php';
+	require dirname( __FILE__ ) . '/../dts_controller.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
